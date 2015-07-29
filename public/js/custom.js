421f315420191fe0aab0e89444b5f234a62c0018
sazanrjb <sazanrjb@gmail.com>
2015-07-29 16:48:40 +0545

    success

--- public/js/custom.js
+++ public/js/custom.js
@@ -7,11 +7,55 @@ var app = angular.module('myApp',[],function($interpolateProvider) {
 });
 
 app.controller('myCtrl',function($scope,$http){
-    var ownerName = $scope.ownerName;
-    var branch = $scope.branch;
-    var multiple = $scope.multiple;
+    select();
+    $scope.insert = function(){
+        $http.post('/r_owner',{
+            ownerName:$scope.ownerName,branch:$scope.branch,multiple:$scope.multiple
+        }).success(function(data,status,headers,config){
+            console.log(data);
+        });
+        select();
+    }
 
-    $http.post('')
+
+    function select(){
+        var names = $http.get('http://localhost:8000/r_owner/display')
+            .success(function(response){
+                $scope.infos = response.records;
+            });
+    }
+
+    $scope.edit = true;
+    $scope.ownerName = "";
+    $scope.branch = "";
+    $scope.editOwner = function(id){
+
+        if(id == 'new'){
+            $scope.edit = true;
+            $scope.ownerName = "";
+            $scope.branch = "";
+        }else{
+            angular.forEach($scope.infos,function(value,key){
+                if(id == value.id){
+                    $scope.ownerName = value.owner_name;
+                    if(value.branch == 1){
+
+                    }else{
+
+                    }
+                }
+                $scope.edit=false;
+            });
+        }
+    }
+
+    $scope.deleteOwner = function(id){
+        $http.delete('/r_owner/'+id).success(function(data,status,headers,config){
+            console.log(data);
+        });
+        select();
+    }
+
 });
 
 $(document).ready(function(){
